11fc32bdaf937a1efbad8a8bbf171cc257f1faaf
Angel de Jesús Castillo Castro <134096082+AngelC123@users.noreply.github.com>
2023-05-19 21:00:05 -0600

    Add files via upload

---
+++ presentacion/OpcionMenu.java
@@ -0,0 +1,15 @@
+package presentacion;
+
+public class OpcionMenu {
+    private short idOpcion;
+    private String texto;
+    private String color;
+    
+    public short getIdOpcion() {
+        return idOpcion;
+    }
+    
+    public void setIdOpcion() {
+        
+    }
+}
