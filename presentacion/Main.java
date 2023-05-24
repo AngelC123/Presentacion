b8cda0c9e8d4c78c156e390282b301ad415dd1b0
Aaron Salsasss <92610252+Salsasss@users.noreply.github.com>
2023-05-24 12:19:44 +0000

    Se modifico el Main para hacer pruebas con los colores

--- presentacion/Main.java
+++ presentacion/Main.java
@@ -1,25 +1,19 @@
 public class Main{
     public static void main(String[] args){
-		OpcionMenu opcion1 = new OpcionMenu((short)1, "Registrar", "ff0000");
-		OpcionMenu opcion2 = new OpcionMenu((short)2, "Imprimir", "ff0000");			
-		OpcionMenu opcion3 = new OpcionMenu((short)3, "Enviar", "ff0000");
-		OpcionMenu opcion4 = new OpcionMenu((short)4, "Graficas", "ff0000");
-		OpcionMenu opcion5 = new OpcionMenu((short)5, "Salir", "ff0000");
-
 		Menu menu = new Menu("Menu Principal",'-');
-		menu.agregarOpcion(opcion1);
-		menu.agregarOpcion(opcion2);
-		menu.agregarOpcion(opcion3);
-		menu.agregarOpcion(opcion4);
-        menu.agregarOpcion(opcion5);
-        menu.desplegar();
+		menu.agregarOpcion((short)1, "Registrar", Colores.verde);
+		menu.agregarOpcion((short)2, "Imprimir", Colores.verde);
+		menu.agregarOpcion((short)3, "Enviar", Colores.verde);
+		menu.agregarOpcion((short)4, "Graficas", Colores.verde);
+		menu.agregarOpcion((short)5, "Salir", Colores.rojo);
+		menu.desplegar();
 
-        System.out.println();        
+		System.out.println();
 		menu.eliminarOpcion((short)2);
-        menu.desplegar();
+		menu.desplegar();
 
-        System.out.println();
-        menu.eliminarOpcion((short)4);
-        menu.desplegar();
+		System.out.println();
+		menu.eliminarOpcion((short)4);
+		menu.desplegar();
 	}
 }
