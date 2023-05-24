863c1788c9e842470814a59666714a1a850a85c8
Angel de Jesús Castillo Castro <134096082+AngelC123@users.noreply.github.com>
2023-05-24 10:42:22 -0600

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import presentacion.Colores;
+import presentacion.Menu;
+
+public class Main{
+    public static void main(String[] args){
+		Menu menu = new Menu("Menu Principal",'-');
+		menu.agregarOpcion((short)1, "Registrar", Colores.verde);
+		menu.agregarOpcion((short)2, "Imprimir", Colores.verde);
+		menu.agregarOpcion((short)3, "Enviar", Colores.verde);
+		menu.agregarOpcion((short)4, "Graficas", Colores.verde);
+		menu.agregarOpcion((short)5, "Salir", Colores.rojo);
+		menu.desplegar();
+
+		System.out.println();
+		menu.eliminarOpcion((short)2);
+		menu.desplegar();
+
+		System.out.println();
+		menu.eliminarOpcion((short)4);
+		menu.desplegar();
+	}
+}
